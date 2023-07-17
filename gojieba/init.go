5f2a4cbbcaa705c60e97b4debb9734af1b36ee56
jiajunxi <252389238@qq.com>
2023-07-17 16:43:28 +0800

    提交初始化错误

--- gojieba/init.go
+++ gojieba/init.go
@@ -1,9 +1,9 @@
 package gojieba
 
 import (
-	"xiezhi/gojieba/deps/cppjieba"
-	"xiezhi/gojieba/deps/limonp"
-	"xiezhi/gojieba/dict"
+	"github.com/cb252389238/xiezhi/gojieba/deps/cppjieba"
+	"github.com/cb252389238/xiezhi/gojieba/deps/limonp"
+	"github.com/cb252389238/xiezhi/gojieba/dict"
 )
 
 func init() {
